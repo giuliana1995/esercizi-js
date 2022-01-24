5735bd3669688dc1928b20f97738797597473a02
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-01-24 09:35:04 +0100

    Add files via upload

---
+++ exercise-3.js
@@ -0,0 +1,29 @@
+function createStore() {
+  const newArray = [];
+  return (product) => {
+    newArray.push(product);
+    console.log(newArray);
+  }
+}
+
+const redPants = {
+  id: 1,
+  name: 'Red Pants'
+};
+const whiteHat = {
+  id: 2,
+  name: 'White Hat'
+};
+const blackSneakers = {
+  id: 3,
+  name: 'Black Sneakers'
+};
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
