caf45f9d3742ef439a68826b0962b262f31177b3
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-01-27 17:50:45 +0100

    Add files via upload

---
+++ exercise-2.js
@@ -0,0 +1,14 @@
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    return this.firstName + " " + this.lastName;
+  }
+
+}
+
+const john = new Person('John', 'Doe');
+const simon = new Person('Simon', 'Collins');
+
+console.log(john.fullName());
+console.log(simon.fullName());
