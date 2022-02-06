daf8856053269c2d7119a75bd13498c5dd1e88d6
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-02-06 23:19:19 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# esercizi-js
+# Classes - Exercise 6
+Definire la classe `AreaCalculator` che, tramite un metodo statico, si occupa di calcolare l'area della figura passata come parametro. Il metodo deve essere capace di calcolare l'area del quadrato, rettangolo e cerchio.
---
+++ exercise-6.js
@@ -0,0 +1,39 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(figure) {
+    switch (figure) {
+      case square:
+        return square.side * square.side;
+      case rectangle: 
+        return rectangle.height * rectangle.width;
+      case circle: 
+        return 2 * 3.14 * circle.radius;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
