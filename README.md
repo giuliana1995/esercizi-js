fe4c39926b049790e46580e4bbdb2f057c2be566
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-02-17 15:22:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# esercizi-js
+# Error handling - Exercise 2
+L'esercizio precedente lanciava un errore durante la fase di prelevamento. In questo esercizio occorre gestire l'errore per evitare il crash dell'applicazione.
---
+++ exercise-2.js
@@ -0,0 +1,47 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    if (amount < 0) {
+      throw new Error('The amount provided cannot be negative');
+    }
+
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    try {
+
+      if (amount < 0) {
+        throw new Error('The amount provided cannot be negative');
+      }
+  
+      if (this.#amount < amount) {
+        throw new Error('You cannot withdraw more than account balance');
+      }
+
+      this.#amount -= amount;
+    }
+    catch (err) {
+      console.log('name', err.name);
+      console.log('message', err.message);
+      console.log('stack', err.stack);
+    }
+
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+// Handle errors to avoid app crash
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(10000);
+bankAccount.view();
