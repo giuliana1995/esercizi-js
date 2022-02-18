d001ecc2beea42123aea4376fe184b1d79a8fc86
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-02-18 17:04:44 +0100

    Add files via upload

---
+++ exercise-4.js
@@ -0,0 +1,12 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+for (let [key, value] of Object.entries(person)) {
+  console.log(`${key}: ${value}`);
+}
+
+
