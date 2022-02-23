c7233f513e206afaf06afc0f376287c1da0d2b92
giulianagiacomino <92444431+giuliana1995@users.noreply.github.com>
2022-02-23 18:10:22 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# esercizi-js
+# Async Management - Exercise 2
+Migliorare la funzione `fetchPersonById`, in modo tale che la `Promise` venga rigettata qualora non esiste una persona con l'id passato come parametro.
---
+++ exercise-2.js
@@ -0,0 +1,37 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const personId = persons.find(person => person.id === id)
+      if(personId) {
+        resolve(personId)
+      } else {
+        reject(new Error(`Non e' stato trovata nessuna persona con id ${id}`))
+      }
+    }, 1000);
+  })
+}
+
+fetchPersonById(2).then((person) => console.log(person)) 
+fetchPersonById(4).then((person) => console.log(person)).catch((err) => {console.log (err) });
